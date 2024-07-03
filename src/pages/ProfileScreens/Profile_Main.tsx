89ef4163192e07197a8d7f7eed8f1b63af0a7b35
Ahmet Furkan Sayan <ahmet.sayan331@gmail.com>
2024-07-03 22:52:38 +0300

    new features for frontend

--- src/pages/ProfileScreens/Profile_Main.tsx
+++ src/pages/ProfileScreens/Profile_Main.tsx
@@ -1,62 +1,107 @@
 import { ScrollView, View } from 'react-native';
 import React, { useState } from 'react';
 import styles from '../../styles/Profile_Main';
-import { Button, Card, IconButton, Text, TextInput } from 'react-native-paper';
+import { Button, Dialog, Card, IconButton, Text, TextInput, PaperProvider, Portal, MD3Colors } from 'react-native-paper';
 import DatePicker from 'react-native-date-picker';
 
 const Profile_Main = () => {
 
     const [adSoyad, setadSoyad] = useState('adsadas');
-    const [dogumTarihi, setdogumTarihi] = useState("");
+    const [tcNo, settcNo] = useState('12312312311');
+    const [dogumTarihi, setdogumTarihi] = useState(new Date());
+    const [open, setOpen] = useState(false);
+    const [kullaniciResmi, setkullaniciResmi] = useState(null);
+
+    const resimSec = () => {
+
+    };
+
+
+    const [visible, setVisible] = useState(false);
+    const showDialog = () => setVisible(true);
+    const hideDialog = () => setVisible(false);
 
     return (
-        <ScrollView style={styles.container} >
-            <Card style={{ backgroundColor: 'white', margin: '5%' }}>
-                <Card.Title titleVariant="titleLarge" titleStyle={{ textAlign: 'center' }} title="Profil Ayarları" />
+        <PaperProvider>
+            <Portal>
+                <Dialog visible={visible} onDismiss={hideDialog}>
+                    <Dialog.Title style={{ textAlign: 'center' }} >Resim İçin Kaynak Seçiniz</Dialog.Title>
+                    <Dialog.Actions style={{ alignSelf: 'center' }}>
+                        <IconButton
+                            icon="camera"
+                            mode="outlined"
+                            iconColor={MD3Colors.error50}
+                            size={50}
+                            onPress={() => resimSec}
+                        />
+                        <IconButton
+                            icon="image-area"
+                            mode="outlined"
+                            iconColor={MD3Colors.error50}
+                            size={50}
+                            onPress={() => resimSec}
+                        />
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
+            <ScrollView style={styles.container} >
+                <Card style={{ backgroundColor: 'white', margin: '5%' }}>
+                    <Card.Title titleVariant="titleLarge" titleStyle={{ textAlign: 'center' }} title="Profil Ayarları" />
 
-                <View style={{ alignItems: 'center' }}>
-                    <Card.Cover style={{ width: 100, height: 100, backgroundColor: '#d9d9d9' }} source={require('../../img/user.png')} />
-                    <Button icon="camera"
-                        style={{ borderColor: '#5A89FF', width: '30%', marginVertical: '2%' }}
-                        mode="outlined"
-                        textColor="#5A89FF"
-                        onPress={() => console.log('object')}
-                    >Change</Button>
-                </View>
-                <Card.Content style={{ marginVertical: '2%', padding: 5, }} >
-                    <TextInput
+                    <View style={{ alignItems: 'center' }}>
+                        <Card.Cover style={{ width: 100, height: 100, backgroundColor: '#d9d9d9' }} source={require('../../img/user.png')} />
+                        <Button icon="camera"
+                            style={{ borderColor: '#5A89FF', width: '60%', marginVertical: '2%' }}
+                            onPress={showDialog}
+                            mode="outlined"
+                            textColor="#5A89FF"
 
-                        label="Ad-Soyad"
-                        disabled={true}
-                        value={adSoyad}
-                        inputMode="text"
-                        mode="outlined" style={{ backgroundColor: 'white', marginBottom: 5 }} />
-                    <DatePicker >
+                        >RESMİ GÜNCELLE</Button>
+                    </View>
+                    <Card.Content style={{ marginVertical: '2%', padding: 5 }} >
+                        <TextInput
+                            label="Ad-Soyad"
+                            disabled={true}
+                            value={adSoyad}
+                            inputMode="text"
+                            mode="outlined" style={{ backgroundColor: 'white', marginBottom: 5 }} />
+                        <TextInput
+                            label="T.C No"
+                            disabled={true}
+                            value={tcNo}
+                            inputMode="decimal"
+                            mode="outlined" style={{ backgroundColor: 'white', marginBottom: 5 }} />
+                        <TextInput
+                            label="Doğum Tarihi"
 
-                    </DatePicker>
-                    <TextInput
-                        label="Name"
-                        value={adSoyad}
-                        inputMode="text"
-                        mode="outlined" style={{ backgroundColor: 'white', marginBottom: 5 }} />
-                    <TextInput
-                        label="Name"
-                        disabled={true}
-                        value={adSoyad}
-                        inputMode="text"
-                        mode="outlined" style={{ backgroundColor: 'white', marginBottom: 5 }} />
-                    <TextInput
-                        label="Name"
-                        disabled={true}
-                        value={adSoyad}
-                        inputMode="text"
-                        mode="outlined" style={{ backgroundColor: 'white' }} />
-                </Card.Content>
-                <Card.Actions>
-                    <Button style={{ backgroundColor: '#5A89FF' }} mode="contained">KAYDET</Button>
-                </Card.Actions>
-            </Card>
-        </ScrollView >
+                            right={<TextInput.Icon
+                                onPress={() => setOpen(true)}
+                                icon="calendar" />}
+                            editable={false}
+                            value={dogumTarihi.toLocaleDateString()}
+                            mode="outlined" style={{ backgroundColor: 'white', marginBottom: 5 }} />
+                        <DatePicker
+                            modal
+                            open={open}
+                            date={dogumTarihi}
+                            mode="date"
+                            onConfirm={(date) => {
+                                setOpen(false);
+                                setdogumTarihi(date);
+                            }}
+                            onCancel={() => {
+                                setOpen(false);
+                            }}
+                        />
+
+                    </Card.Content>
+                    <Card.Actions>
+                        <Button style={{ backgroundColor: '#5A89FF' }} mode="contained">KAYDET</Button>
+                    </Card.Actions>
+                </Card>
+            </ScrollView >
+        </PaperProvider>
+
     );
 };
 
